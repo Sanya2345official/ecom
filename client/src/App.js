2ec921c340db3b6fc4dfe991a6fc94d532fa393d
Sanya2345official <sanya2345.official@gmail.com>
2025-06-20 21:49:44 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -8,18 +8,38 @@ function App() {
   return (
     <Router>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="container">
-          <Link className="navbar-brand" to="/">MyShop</Link>
-          <div className="collapse navbar-collapse">
-            <ul className="navbar-nav ms-auto">
-              <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
-              <li className="nav-item"><Link className="nav-link" to="/products">Products</Link></li>
-              <li className="nav-item"><Link className="nav-link" to="/cart">Cart</Link></li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+      <div className="container">
+        <Link className="navbar-brand" to="/">MyShop</Link>
 
+        {/* Toggler button */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* Collapsible content */}
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/products">Products</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/cart">Cart</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
       <div className="container mt-4">
         <Routes>
           <Route path="/" element={<Home />} />
