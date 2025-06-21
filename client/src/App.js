e0bbeaed4dbe8db4d4b4a2f05ffc2eb4d6ca5dec
Sanjeev <tradesfolks@gmail.com>
2025-06-21 11:05:40 +0530

    Your commit message describing the changes

--- client/src/App.js
+++ client/src/App.js
@@ -10,11 +10,32 @@ function App() {
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container">
           <Link className="navbar-brand" to="/">MyShop</Link>
-          <div className="collapse navbar-collapse">
+
+          {/* Toggler button */}
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          {/* Collapsible content */}
+          <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav ms-auto">
-              <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
-              <li className="nav-item"><Link className="nav-link" to="/products">Products</Link></li>
-              <li className="nav-item"><Link className="nav-link" to="/cart">Cart</Link></li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/">Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/products">Products</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/cart">Cart</Link>
+              </li>
             </ul>
           </div>
         </div>
